3c104155465dc37a2a3bc00e4b03ea71eedd49fc
mmzhhhh <254995030@QQ.com>
2024-09-25 10:37:55 +0800

    delete invoices func

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,4 @@
+import { deleteInvoice } from '@/app/';
 "use server";
 
 import { z } from "zod";
@@ -52,3 +53,10 @@ export async function updateInvoice(id: string, formData: FormData) {
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
+
+export async function deleteInvoice(id:string) {
+  await sql`DELETE FROM invoices WHERE id=${id}`;
+  revalidatePath('/dashboard/invoices');
+}
+
+
